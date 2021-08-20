b887b86c0f4c597f8df29bc35b7be1b4fbff27c3
allelaok <88119690+allelaok@users.noreply.github.com>
2021-08-20 11:57:10 +0900

    x

---
+++ Assets/NSR/01.Na_Scripts/Na_Player_move.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Na_Player_move : MonoBehaviour
+{
+    public float speed = 7f;
+    CharacterController cc;
+
+    public float jumpPower = 3f;
+    float yVelocity;
+    public float gravity = -7f;
+    Vector3 dir;
+
+    int jumpCount;
+    public int MaxJumpCount = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 dirH = transform.right * h;
+        Vector3 dirV = transform.forward * v;
+
+        dir = dirH + dirV;
+        dir.Normalize();
+
+
+        Jump();
+
+        cc.Move(dir * speed * Time.deltaTime);
+
+    }
+
+
+    void Jump()
+    {
+        if (cc.isGrounded)
+        {
+            yVelocity = 0;
+            jumpCount = 0;
+        }
+
+        if (Input.GetButtonDown("Jump") || Input.GetMouseButtonDown(1))
+        {
+            if (jumpCount < MaxJumpCount)
+            {
+                yVelocity = jumpPower;
+                jumpCount++;
+            }
+
+
+        }
+        dir.y = yVelocity;
+        yVelocity += gravity * Time.deltaTime;
+
+
+    }
+}
---
+++ Assets/NSR/01.Na_Scripts/Na_Rotate.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Na_Rotate : MonoBehaviour
+{
+    public float rotSpeed = 200f;
+
+    float x;
+    float y;
+
+    public bool onV;
+    public bool onH;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = transform.localEulerAngles.x;
+        y = transform.localEulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        if(onV)
+        x += v * rotSpeed * Time.deltaTime;
+        if(onH)
+        y += h * rotSpeed * Time.deltaTime;
+
+        x = Mathf.Clamp(x, -30, 30);
+
+        transform.localEulerAngles = new Vector3(-x, y, 0);
+    }
+}
