b50bc5eb1e2b98156c94917227f7f4f52d122b6f
jkh5039 <borm3403@naver.com>
2021-08-24 17:13:22 +0900

    .c

--- Assets/JKH/01.KH_Scrips/KH_EnemyFire.cs
+++ Assets/JKH/01.KH_Scrips/KH_EnemyFire.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class KH_EnemyFire : MonoBehaviour
 {
     float currTime;
-    float gunDuration = .2f;
+    float gunDuration = 0.2f;
     public GameObject bulletFactory;
     public GameObject firePos;
     public GameObject target;
@@ -20,12 +20,16 @@ public class KH_EnemyFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        enemyShot();
+    }
+
+    void enemyShot()
+    {
         Vector3 dir = target.transform.position - transform.position; //에너미가 바라보는방향
         dir.Normalize(); //정규화
         //transform.LookAt(target.transform); //에너미가 타겟을 바라본다
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), rotSpeed * Time.deltaTime); //몸을돌린다        
-        currTime += Time.deltaTime; //시간이흐르는데
-        
+        currTime += Time.deltaTime; //시간이흐르는데        
         if (gunDuration <= currTime) //총알발사속도
         {
             GameObject bullet = Instantiate(bulletFactory); //총알을 생성한다
@@ -38,22 +42,70 @@ public class KH_EnemyFire : MonoBehaviour
             Destroy(bullet, 3); //일정시간 지나면 파괴한다
             currTime = 0; //현재시간 초기화
         }
-        
     }
+    public float AttackRange = 15f;
+    void findPlayer()
+    {
+        Vector3 dir = target.transform.position - transform.position; //P-E
+        float distance = dir.magnitude; //P-E거리
+        if (distance<AttackRange) //만약 player가 사정거리 안에 들어왔다면?
+        {
+            //몸을 그방향으로 꺾는다 
+            Vector3 dirE = target.transform.position - transform.position; //에너미가 바라보는방향
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dirE),
+                rotSpeed * Time.deltaTime); //몸을돌린다
+            //만약 ray를 발사해서 player가 잡힌다면?
+            //(무빙치며) 할까말까
+            //레이저를쏜다 (이건 딜 안들어감, 희망사항)
+            //총알을 발사한다.
+        }
+
+        else
+        {
+            //원상태로 돌아간다
+        }
+
+
+        //만약 일정범위안에a 적군을 인식한다면(거리로? ray로? 벽어떻게하뮤ㅠㅠ) 
+        //정찰을 멈춘다(공격모드)
+        //몸과 총구를 상대방쪽으로 꺾는다
+        //레이가 상대방에게 닿은다면? (벽 안뚫리게 해야함)
+        //총을 발사한다 (위에코드)
+        //만약 일정범위 밖에 있다면 
+        //원상태(정찰모드)로 바꾼다
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     void justMemo()
     {
-        //Ray?? 를발사 
-        //에너미가 일정구간을 정찰한다 (정찰모드)
-        //-해당방향으로 움직인다
-        //-해당방향에 도착한다면
-        //-반대방향으로 움직인다 
-        //-반대방향에 도착하면 또 다시 반복
-        //만약 일정범위안에 적군을 인식한다면(거리로? ray로? 벽어떻게하뮤ㅠㅠ) 
-        //정찰을 멈춘다
+        //Enemy가 정찰하다가 player위치에 반응하고 총구를 플레이어를 향한다음 쏜다 
+
+        //에너미가 일정구간을 정찰한다 (정찰모드) //navi
+        //- 방향 두개찍고 와리가리 치기
+        //만약 일정범위안에a 적군을 인식한다면(거리로? ray로? 벽어떻게하뮤ㅠㅠ) 
+        //정찰을 멈춘다(공격모드)
         //몸과 총구를 상대방쪽으로 꺾는다
         //레이가 상대방에게 닿은다면? (벽 안뚫리게 해야함)
-        //(살짝살짝 움직이면서) 총을 발사한다 (위에코드)
+        //총을 발사한다 (위에코드)
         //만약 일정범위 밖에 있다면 
         //원상태(정찰모드)로 바꾼다
 
