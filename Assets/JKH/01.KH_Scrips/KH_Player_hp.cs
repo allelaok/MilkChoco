c1648ab983a56a730f1ebd41c6622c46e012c945
jkh5039 <borm3403@naver.com>
2021-08-30 14:29:59 +0900

    Update KH_Player_hp.cs

--- Assets/JKH/01.KH_Scrips/KH_Player_hp.cs
+++ Assets/JKH/01.KH_Scrips/KH_Player_hp.cs
@@ -8,6 +8,11 @@ public class KH_Player_hp : MonoBehaviour
     public float currHP;
     public float maxHP = 100;
     public Image hpUI;
+    GameObject ismilk; //내가 먹은우유
+    public Transform milkPos; //근데 이거 4개있잖음... 나중에 조정한다
+    public Transform EnemyStartPos;
+    float currTIme;
+    float bornTime=15;
     // Start is called before the first frame update
     void Start()
     {
@@ -27,8 +32,19 @@ public class KH_Player_hp : MonoBehaviour
 
         if (currHP <= 0) //currHp가 0이라면 
         {
+            gameObject.transform.position = EnemyStartPos.transform.position;
+            if (ismilk)
+            {
+                //우유정위치
+            }
             gameObject.SetActive(false);
-            Na_DestroyZone.instance.playerDie = true;
+            currTIme += Time.deltaTime;
+            if (currTIme > bornTime)
+            {
+                gameObject.SetActive(false);
+                //다른스크립트에서 이거 state바꾼다.
+            }
+            
         }
     }
 }
