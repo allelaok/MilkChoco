cb49183d5a895c12b51b85a7a182928c0b97b0e0
jkh5039 <borm3403@naver.com>
2021-09-07 17:27:44 +0900

    asasas

--- Assets/JKH/01.KH_Scrips/KH_GameManager.cs
+++ Assets/JKH/01.KH_Scrips/KH_GameManager.cs
@@ -73,6 +73,13 @@ public class KH_GameManager : MonoBehaviour
             i++;
             enemyStart[numbers[i]].SetActive(true);
             isChoco = false;
+
+
+            //초코 컨테이너 계속 채워야한다
+            //attack move에서 더해진 chocoCount를 가지고와서
+            //초코컨테이너에다가 초코를 채운다
+            //만약 초코카운트가 3이된다면?
+            //END Scene을 뽑는다.
         }
     }
 }
