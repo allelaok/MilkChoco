6576f3ca88d2d9b3f3deda13de69c7e73526d9b7
allelaok <88119690+allelaok@users.noreply.github.com>
2021-09-16 13:00:31 +0900

    DieIMG

---
+++ Assets/NSR/01.Na_Scripts/Na_DieAnim.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Na_DieAnim : MonoBehaviour
+{
+    public void DieIMG()
+    {
+        Na_Player.instace.DieIMG();
+    }
+}
