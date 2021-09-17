9edd44bf5d500f3a8034ffd498a252ff5a8c2304
Hongdori <88076989+Hongdori@users.noreply.github.com>
2021-09-17 11:09:23 +0900

    ..

--- Assets/HSM/SM_Scripts/SM_StartScene.cs
+++ Assets/HSM/SM_Scripts/SM_StartScene.cs
@@ -3,32 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Playables;
+using Cinemachine;
 
 
 public class SM_StartScene : MonoBehaviour
 {
+    PlayableDirector pd;
 
     public GameObject gameStart;
+
+    public GameObject gameExit;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        pd = GetComponent<PlayableDirector>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
-        {
-            SceneManager.LoadScene("MainScene");
-        }
+       
 
-        void OnClickButton()
-        {
+        //void OnClickButton()
+        //{
 
-        }
+        //}
 
+        //void OnClickButton()
+        //{
 
+        //}
     }
 }
